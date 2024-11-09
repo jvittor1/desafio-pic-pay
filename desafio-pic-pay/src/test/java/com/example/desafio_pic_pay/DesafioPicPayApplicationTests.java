e30bf1f5577a4dc2652c532bff36d1663e139684
João Vitor <joaovitorb3105@gmail.com>
2024-11-08 22:56:13 -0300

    init commit

---
+++ desafio-pic-pay/src/test/java/com/example/desafio_pic_pay/DesafioPicPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_pic_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
